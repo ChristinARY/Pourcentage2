538766bc5f6e79093587cbbd451bdfdd8d364557
Christin <arychristin@gmail.com>
2020-01-24 15:45:51 +0100

    Initial Commit

---
+++ Pourcentage2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pourcentage2
+//
+//  Created by Christin Adeomi Rafiou YESSOUFOU on 24/01/2020.
+//  Copyright © 2020 Christin Adeomi Rafiou YESSOUFOU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
